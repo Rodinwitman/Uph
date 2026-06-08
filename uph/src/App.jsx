fa0ecf72ec92a1e19c06bd7ad2b6c675a59579b9
rodin <rodinwitman9@gmail.com>
2026-06-09 00:42:57 +0300

    initiation project

---
+++ uph/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-blue-500">
+      Tailwind fonctionne ! 🎉
+    </h1>
+    <p className='text-8xl'>test</p>
+    </>
+  )
+}
+
+export default App
